6eee6167a0b8ca9b9674b78f7a00dfd9f763c5df
Less <qkqkhih55@gmail.com>
2024-05-15 18:58:30 +0900

    practice

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment';
 
+const comments =[
+  {name:'NLess', content: 'My Comment!'},
+  {name:'Largo', content: 'Do Neck, too.'},
+  {name:'Brim', content: '세상에..샐러드가..감자탕이..'},
+];
 function App() {
   return (
-    <div className="App">
+    <div className="App" style ={{padding:16, backgroundColor:'#282c34'}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +25,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {comments.map((comment,index)=>{
+          return(
+            <Comment
+              name={comment.name}
+              content={comment.content}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
